2230788af4b1199e13006458e806da733517320f
Abhay Ekanath <abhayekanath@gmail.com>
2025-05-25 19:07:44 +0530

    Removes commented-out code

--- Sample/tasks.js
+++ Sample/tasks.js
@@ -5,7 +5,6 @@ num2 = 10;
 function sum(num1, num2) {
   return num1 + num2;
 }
-
 // console.log(sum(num1,num2));
 
 //2. Write a javascript function to find the difference, product and division of two numbers.
